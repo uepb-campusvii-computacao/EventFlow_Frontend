31d311a70cf9bbb026d962d9315d9783b5d75fd0
Artur Dantas <arturdantasrodrigues@gmail.com>
2024-08-26 22:31:08 -0300

    fix: ajusta responsividade do header em telas maiores | ajusta lista de eventos para ocupar todo o espaço do container

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,21 +1,25 @@
 import { Link } from 'react-router-dom';
 import { MobileNavBar } from './MobileNavBar';
 import { NavBar } from './NavBar';
+import { Container } from './Container';
 
 export function Header() {
   return (
-    <header className="flex h-16 px-8 w-full items-center justify-between border-b-[4px] border-b-accent">
-      <Link to="/">
-        <img
-          width={64}
-          height={64}
-          src="/vite.svg"
-          className="h-8 w-auto max-sm:h-6"
-          alt="logo"
-        />
-      </Link>
-      <NavBar />
-      <MobileNavBar /> {/* its hidden until the width got 768px */}
+    <header className="w-full border-b-[4px] border-b-accent">
+      <Container className="flex items-center justify-between h-16"> 
+        <Link to="/">
+          <img
+            width={64}
+            height={64}
+            src="/vite.svg"
+            className="h-8 w-auto max-sm:h-6"
+            alt="logo"
+          />
+        </Link>
+        <NavBar />
+        <MobileNavBar /> {/* its hidden until the width got 768px */}
+      </Container>
+      
     </header>
   );
 }
