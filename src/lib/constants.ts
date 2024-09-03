30fe4928fbd3ffd9169148f8c3fb5d9da7d3d088
Artur Dantas <arturdantasrodrigues@gmail.com>
2024-09-02 23:38:58 -0300

    EV-8 feat: adiciona nova estilização em todas as seções da home page

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -55,106 +55,25 @@ export const navbar_links: { title: string; href: string }[] = [
   },
 ];
 
-export const events_data = [
-  {
-    id: 1,
-    name: 'VI SERCOMP Congresso Sertanejo de Computação',
-    image_url:
-      'https://even3.blob.core.windows.net/banner/CAPAPARASITE.72d02a733c034fb18316.png',
-    location: 'New York, USA',
-    date: '2024-07-23',
-  },
-  {
-    id: 2,
-    name: 'Jane Smith',
-    image_url:
-      'https://even3.blob.core.windows.net/banner/CAPAPARASITE.72d02a733c034fb18316.png',
-    location: 'Los Angeles, USA',
-    date: '2024-07-22',
-  },
-  {
-    id: 3,
-    name: 'Carlos Santos',
-    image_url:
-      'https://even3.blob.core.windows.net/banner/CAPAPARASITE.72d02a733c034fb18316.png',
-    location: 'São Paulo, Brazil',
-    date: '2024-07-21',
-  },
-  {
-    id: 4,
-    name: 'Maria Garcia',
-    image_url:
-      'https://even3.blob.core.windows.net/banner/CAPAPARASITE.72d02a733c034fb18316.png',
-    location: 'Madrid, Spain',
-    date: '2024-07-20',
-  },
-  {
-    id: 5,
-    name: 'Akira Tanaka',
-    image_url:
-      'https://even3.blob.core.windows.net/banner/CAPAPARASITE.72d02a733c034fb18316.pnghttps://example.com/images/akiratanaka.jpg',
-    location: 'Tokyo, Japan',
-    date: '2024-07-19',
-  },
-  {
-    id: 6,
-    name: "Liam O'Connor",
-    image_url:
-      'https://even3.blob.core.windows.net/banner/CAPAPARASITE.72d02a733c034fb18316.png',
-    location: 'Dublin, Ireland',
-    date: '2024-07-18',
-  },
-  {
-    id: 7,
-    name: 'Emma Müller',
-    image_url:
-      'https://even3.blob.core.windows.net/banner/CAPAPARASITE.72d02a733c034fb18316.png',
-    location: 'Berlin, Germany',
-    date: '2024-07-17',
-  },
-  {
-    id: 8,
-    name: 'Pierre Dubois',
-    image_url:
-      'https://even3.blob.core.windows.net/banner/CAPAPARASITE.72d02a733c034fb18316.png',
-    location: 'Paris, France',
-    date: '2024-07-16',
-  },
-  {
-    id: 9,
-    name: 'Chen Wei',
-    image_url:
-      'https://even3.blob.core.windows.net/banner/CAPAPARASITE.72d02a733c034fb18316.pnghttps://example.com/images/chenwei.jpg',
-    location: 'Beijing, China',
-    date: '2024-07-15',
-  },
-  {
-    id: 10,
-    name: 'Olivia Johnson',
-    image_url:
-      'https://even3.blob.core.windows.net/banner/CAPAPARASITE.72d02a733c034fb18316.png',
-    location: 'Sydney, Australia',
-    date: '2024-07-14',
-  },
-];
 
-export const events_batchs = [
-  {
-    id: '1',
-    name: 'Evento A',
-    description: 'Descrição do Evento A',
-    price: 90.0,
-  },
-  {
-    id: '2',
-    name: 'Evento B',
-    description: 'Descrição do Evento B',
-    price: 90.0,
-  },
-  {
-    id: '3',
-    name: 'Evento C',
-    description: 'Descrição do Evento C',
-    price: 90.0,
-  },
-];
+export const recursos = [{
+  imgUrl: './src/assets/message-icon.png',
+  imgAlt: 'Ícone de mensagem com um usuário',
+  title: 'Gerenciamento de Inscrições',
+  description: 'Otimize as inscrições e o acompanhamento dos participantes, garantindo uma experiência fluida para os usuários.'
+}, {
+  imgUrl: './src/assets/calendar-icon.png',
+  imgAlt: 'Ícone de calendário com marcador de localização',
+  title: 'Agendamento de Locais',
+  description: 'Simplifique a reserva de espaços, agendamento de datas e horários, melhorando o funcionamento do evento.'
+}, {
+  imgUrl: './src/assets/activities-icon.png',
+  imgAlt: 'Ícone de três listas de tarefas',
+  title: 'Gestão de Atividades e Suporte',
+  description: 'Coordene e organize todas as atividades e sessões do evento, garantindo um cronograma claro e eficiente.'
+}, {
+  imgUrl: './src/assets/graph-icon.png',
+  imgAlt: 'Ícone de gráfico com linha ascendente',
+  title: 'Gerenciamento de Relatórios',
+  description: 'Visualize insights detalhados sobre o desempenho do evento, garantindo o aperfeiçoamento de futuras edições.'  
+}]
