58cef6271c1f9057e268eb9e1fb23cd5d2fb0abf
ThulioBezerra <thulioalves20@gmail.com>
2025-03-04 17:21:44 -0300

    refactor: muda nome de componente brickcardmp

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,10 +1,10 @@
-import { BrikcCardMp } from "@/components/shared/BrickMP";
+import { BrickCardMp } from "@/components/shared/BrickMP";
 
 export function Subscribe() {
   return (
     <main className="flex min-h-dvh w-full items-center justify-center bg-accent p-2 md:p-12">
       <div className="lg:w-[468px]">
-      <BrikcCardMp />
+      <BrickCardMp />
       </div>
     </main>
   );
