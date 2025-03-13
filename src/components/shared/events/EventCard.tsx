cecc51acba716995e759dde1e5f90bafbbd63019
ThulioBezerra <thulioalves20@gmail.com>
2025-03-13 18:23:16 -0300

    remove: retira cookies de ui.

--- src/components/shared/events/EventCard.tsx
+++ src/components/shared/events/EventCard.tsx
@@ -14,7 +14,7 @@ import { Button } from "@/components/ui/button";
 import { api } from "@/lib/api";
 import Cookies from "js-cookie";
 import toast from "react-hot-toast";
-import { Cookie, Eye, EyeOff } from 'lucide-react';
+import { Eye, EyeOff } from 'lucide-react';
 
 interface EventCardProps {
   slug: string;
