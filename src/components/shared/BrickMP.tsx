deeb96c2a9f200063db16fdabb4978a343d252c0
Isaque1600 <isaquebatista25@gmail.com>
2025-03-23 02:43:24 -0300

    feat: create new BrickCard to refresh payment

--- src/components/shared/BrickMP.tsx
+++ src/components/shared/BrickMP.tsx
@@ -8,23 +8,22 @@ import {
   ICardPaymentBrickPayer,
   ICardPaymentFormData,
 } from '@mercadopago/sdk-react/esm/bricks/cardPayment/type';
-import { useMutation } from '@tanstack/react-query';
-import { useNavigate, useParams } from 'react-router-dom';
-import { useQueryClient } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import toast from 'react-hot-toast';
+import { useNavigate, useParams } from 'react-router-dom';
 
 initMercadoPago(import.meta.env.VITE_MERCADOPAGO_PUBLIC_KEY);
-async function  registerPayment(loteId: string, paymentData: any) {
-  try{
-  const response = await api.post(`/lote/${loteId}/register`,
-    {paymentData}
-  )
-  return response.data;
+async function registerPayment(loteId: string, paymentData: any) {
+  try {
+    const response = await api.post(`/lote/${loteId}/register`, {
+      paymentData,
+    });
+    return response.data;
   } catch (error) {
     toast.error('Erro ao registrar inscrição');
   }
 }
-  
+
 export function BrickCardMp({
   amount,
   loteId,
@@ -36,16 +35,15 @@ export function BrickCardMp({
     paymentMethods: {
       minInstallments: 1,
       maxInstallments: 12,
-    }
+    },
   };
   const queryClient = useQueryClient();
-  const {mutate} = useMutation({
-     mutationFn: (payload: any)=> registerPayment(loteId, payload)
-     ,
-     mutationKey: ['register-payment'],
-     onSuccess: () => {
-       queryClient.invalidateQueries({queryKey:['user-registration']});
-     }
+  const { mutate } = useMutation({
+    mutationFn: (payload: any) => registerPayment(loteId, payload),
+    mutationKey: ['register-payment'],
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['user-registration'] });
+    },
   });
 
   const navigate = useNavigate();
@@ -62,18 +60,14 @@ export function BrickCardMp({
       paymentMethod: 'CARD',
       paymentData: formData,
     };
-    try{
+    try {
       mutate(payload.paymentData);
-      navigate(`/pagamentos/${slug}`)
-    } 
-    catch (error) {
+      navigate(`/pagamentos/${slug}`);
+    } catch (error) {
       console.error('Erro ao registrar inscrição:', error);
       throw error;
+    } finally {
     }
-    finally {
-    
-    }
-
   };
   const onError = async (error: any) => {
     console.log(error);
@@ -85,7 +79,76 @@ export function BrickCardMp({
       onSubmit={onSubmit}
       customization={customization}
       onError={onError}
+    />
+  );
+}
 
+async function registerNewPayment(loteId: string, paymentData: any) {
+  try {
+    const response = await api.post(`/lote/${loteId}/create-new-payment`, {
+      paymentData,
+    });
+    return response.data;
+  } catch (error) {
+    toast.error('Erro ao registrar novo pagamento');
+  }
+}
+
+export function BrickCardMpRefreshPayment({
+  amount,
+  loteId,
+}: {
+  amount: number;
+  loteId: string;
+}) {
+  const customization = {
+    paymentMethods: {
+      minInstallments: 1,
+      maxInstallments: 12,
+    },
+  };
+  const queryClient = useQueryClient();
+  const { mutate } = useMutation({
+    mutationFn: (payload: any) => registerNewPayment(loteId, payload),
+    mutationKey: ['register-payment'],
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['user-registration'] });
+    },
+  });
+
+  const navigate = useNavigate();
+  const { slug } = useParams();
+  //pegar o preço do lote - nao implementado
+  const initialization = {
+    amount: amount || 0,
+  };
+
+  const onSubmit = async (
+    formData: ICardPaymentFormData<ICardPaymentBrickPayer>
+  ) => {
+    const payload = {
+      paymentMethod: 'CARD',
+      paymentData: formData,
+    };
+    try {
+      mutate(payload.paymentData);
+      navigate(`/pagamentos/${slug}`, { replace: true });
+    } catch (error) {
+      console.error('Erro ao registrar novo pagamento:', error);
+      throw error;
+    }
+  };
+
+  const onError = async (error: any) => {
+    console.log(error);
+  };
+
+  return (
+    <CardPayment
+      initialization={initialization}
+      onSubmit={onSubmit}
+      customization={customization}
+      onError={onError}
     />
   );
 }
